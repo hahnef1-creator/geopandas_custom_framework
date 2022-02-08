19a9288c26243afd124e3f63d98e69e5f43904fd
hahnef1-creator <75389921+hahnef1-creator@users.noreply.github.com>
2022-02-08 15:03:50 -0500

    Update geo.py

--- geo.py
+++ geo.py
@@ -169,7 +169,10 @@ def custom_replicate(input_gdb,output_gdb):
     for name in input_data:
         arcpy.env.workspace = input_gdb
         arcpy.env.OverwriteOutput= True
-        if name.split(".")[-1] in output_names_in_gdb:
+        if input_gdb.split(".")[-1]=="sde":
+            if name.split(".")[-1] in output_names_in_gdb:
+                pass
+        elif name in output_names_in_gdb:
             pass
         else:
             describe = arcpy.Describe(name)
